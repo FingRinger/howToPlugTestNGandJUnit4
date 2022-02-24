184c654bd836dd8cc75ad73d27416311a1e00d6c
Родионова Анастасия Константиновна <a.rodionova@hq.icfed.com>
2022-02-24 14:52:59 +0300

    New tests are added in testNG branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -17,4 +17,40 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual, "Wrong remain is counted");
     }
+
+    @Test
+    public void remainWhenIsMoreThen1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1400;
+        int expected = 600;
+
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual, "Wrong remain is counted");
+    }
+
+    @Test // Bug
+    public void remainWhenIs1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual, "Wrong remain is counted");
+    }
+
+    @Test // Bug
+    public void remainWhenIs2000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 2000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual, "Wrong remain is counted");
+    }
 }
