ed2912367e061ab16363e010021f9ec439b1ace2
Родионова Анастасия Константиновна <a.rodionova@hq.icfed.com>
2022-02-24 15:25:47 +0300

    Tests with Junit4 are added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,50 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void remainWhenLessThen1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainWhenIsMoreThen1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1400;
+        int expected = 600;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test // Bug
+    public void remainWhenIs1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test // Bug
+    public void remainWhenIs2000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 2000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
     }
 }
