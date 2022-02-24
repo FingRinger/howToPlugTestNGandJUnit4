16a8f405246a7ac42d2d5afcbae264ee5ee33899
Родионова Анастасия Константиновна <a.rodionova@hq.icfed.com>
2022-02-24 14:28:07 +0300

    Tests are added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void testRemainIfAmountLessThen1000() {
 
         CashbackHackService service = new CashbackHackService();
 
@@ -17,6 +17,45 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
 
         assertEquals(actual, expected, "Wrong remain is counted");
-
     }
+
+    @Test // Если пользователь купил ровно на 1000 рублей, то приложение не должно ему говорить, что нужно купить ещё на 1000.
+    public void testRemainIfAmountIs1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected, "Wrong remain is counted");
+    }
+
+    @Test
+    public void testRemainIfAmountMoreThen1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1400;
+        int expected = 600;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected, "Wrong remain is counted");
+    }
+
+    @Test
+    public void testRemainIfAmountIs2000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 2000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected, "Wrong remain is counted");
+    }
+
 }
